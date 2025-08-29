6aa647c3b6ca777044d7ee4bdfa6e82a615ba3a1
Badr Elfarri <badr@neate.no>
2025-08-29 22:47:07 +0200

    add retry func

--- rediskit/__init__.py
+++ rediskit/__init__.py
@@ -10,6 +10,7 @@ from rediskit.memoize import redis_memoize
 from rediskit.redis_client import get_async_redis_connection, get_redis_connection, init_async_redis_connection_pool, init_redis_connection_pool
 from rediskit.redis_in_eventloop import get_async_redis_connection_in_eventloop
 from rediskit.redisLock import get_async_redis_mutex_lock, get_redis_mutex_lock
+from rediskit.retry_decorator import RetryPolicy, retry_async
 from rediskit.semaphore import Semaphore
 
 __all__ = [
@@ -24,4 +25,6 @@ __all__ = [
     "Encrypter",
     "Semaphore",
     "AsyncSemaphore",
+    "RetryPolicy",
+    "retry_async",
 ]
